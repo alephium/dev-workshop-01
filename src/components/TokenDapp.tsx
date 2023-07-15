fdef0fb2133ff996f0acef80bfc976e18d852f70
h0ngcha0 <lbs.lhc@gmail.com>
2023-07-15 04:55:50 +0200

    Withdraw from faucet in UI

--- src/components/TokenDapp.tsx
+++ src/components/TokenDapp.tsx
@@ -1,15 +1,39 @@
 import { FC, useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 import { useAlephiumConnectContext } from '@alephium/web3-react'
-import { web3 } from '@alephium/web3'
+import { node, web3 } from '@alephium/web3'
 import { TokenFaucetConfig, tokenFaucetConfig } from '@/services/utils'
+import { withdrawToken } from '@/services/token.service'
 import { TokenFaucet } from 'artifacts/ts'
+import { TxStatus } from './TxStatus'
 
 export const TokenDapp: FC<{
   config: TokenFaucetConfig
 }> = ({ config }) => {
   const context = useAlephiumConnectContext()
+  const addressGroup = config.groupIndex
   const [balance, setBalance] = useState<string | undefined>(undefined)
+  const [withdrawAmount, setWithdrawAmount] = useState('')
+  const [ongoingTxId, setOngoingTxId] = useState<string>()
+
+  const handleWithdrawSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    if (!!context.signerProvider) {
+      const result = await withdrawToken(context.signerProvider, withdrawAmount, config.faucetTokenId)
+      setOngoingTxId(result.txId)
+    }
+  }
+
+  const txStatusCallback = (status: node.TxStatus, numberOfChecks: number): Promise<any> => {
+    if (
+      (status.type === 'Confirmed' && numberOfChecks > 2) ||
+      (status.type === 'TxNotFound' && numberOfChecks > 3)
+    ) {
+      setOngoingTxId(undefined)
+    }
+
+    return Promise.resolve()
+  }
 
   useEffect(() => {
     const handler = async () => {
@@ -23,13 +47,14 @@ export const TokenDapp: FC<{
     }
 
     handler()
-  }, [context.signerProvider?.nodeProvider, tokenFaucetConfig.tokenFaucetAddress])
+  }, [context.signerProvider?.nodeProvider, tokenFaucetConfig.tokenFaucetAddress, ongoingTxId])
 
-  const addressGroup = config.groupIndex
   return (
     <>
+      {ongoingTxId && <TxStatus txId={ongoingTxId} txStatusCallback={txStatusCallback} />}
+
       <div className="columns">
-        <form>
+        <form onSubmit={handleWithdrawSubmit}>
           <>
             <h2 className={styles.title}>Alephium Token Faucet on {config.network}</h2>
             <p>PublicKey: {context.account?.publicKey ?? '???'}</p>
@@ -50,6 +75,18 @@ export const TokenDapp: FC<{
                 </tr>
               </tbody>
             </table>
+            <label htmlFor="withdraw-amount">Amount</label>
+            <input
+              type="number"
+              id="transfer-amount"
+              name="amount"
+              max="2"
+              min="1"
+              value={withdrawAmount}
+              onChange={(e) => setWithdrawAmount(e.target.value)}
+            />
+            <br />
+            <input type="submit" disabled={!!ongoingTxId} value="Send Me Token" />
           </>
         </form>
       </div>
