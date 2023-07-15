ba87c9f9bcab0e2170322f611e4ccfd55f5cbbec
h0ngcha0 <lbs.lhc@gmail.com>
2023-07-15 04:14:25 +0200

    add AlephiumConnectButton

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AlephiumConnectProvider } from '@alephium/web3-react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AlephiumConnectProvider useTheme="web95" network={'devnet'} addressGroup={0}>
+      <Component {...pageProps} />
+    </AlephiumConnectProvider>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
 import { TokenDapp } from '@/components/TokenDapp'
+import { AlephiumConnectButton, useAccount } from '@alephium/web3-react'
 
 export default function Home() {
+  const { account } = useAccount()
+
   return (
     <>
-      <Head>
-        <title>Welcome to the Dev Workshop!</title>
-        <meta name="description" content="Demo app for the Alephium dev workshop" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <TokenDapp />
+      <div className={styles.container}>
+        <AlephiumConnectButton />
+        <Head>
+          <title>Welcome to the Dev Workshop!</title>
+          <meta name="description" content="Demo app for the Alephium dev workshop" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        {!!account && (
+          <div>{account.address}</div>
+        )}
+        <TokenDapp />
+      </div>
     </>
   )
 }
